2d47fc3c0f3f570bcc240b42be19fc83ac6f6dd2
Eldar Badalbeyli <eld.bad@yandex.com>
2025-02-21 13:37:33 +0500

    Initial

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::{
+    env,
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, Write},
+};
+
+fn main() {
+    // Commands
+    // 1. Parse and print last time. If not found - print "not found"
+    // 2. Restart time
+    // 3. View all times
+    // TODO: change unwrap to something more pleasing
+    let args: Vec<String> = env::args().collect();
+    // if args.len() != 2 | 3 {
+    //     panic!("oops");
+    // }
+    if &args[1] == "r" {
+        let my_time = time::OffsetDateTime::now_utc().to_string();
+        println!("{}", my_time);
+        write_to_file(my_time);
+    } else if &args[1] == "l" {
+        println!("{}", read_from_file());
+    } else {
+        panic!("wrong arguments");
+    }
+}
+
+fn read_from_file() -> String {
+    let file = File::open("times.txt").unwrap();
+    let lines = BufReader::new(file).lines();
+    let last_el = lines.last().unwrap().unwrap();
+
+    return last_el;
+}
+
+fn write_to_file(date: String) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("times.txt")
+        .unwrap();
+    file.write((date + "\n").as_bytes()).unwrap();
+}
