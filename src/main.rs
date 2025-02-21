c0ff026e4e2ab0cfc627cab00a7ca95bca8d7b30
Eldar Badalbeyli <eld.bad@yandex.com>
2025-02-21 16:16:50 +0500

    Functions to return Result

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::{
     env,
     fs::{self, File, OpenOptions},
-    io::{BufRead, BufReader, Write},
+    io::{self, BufRead, BufReader, Write},
 };
 
-fn main() {
-    // TODO: change unwrap to something more pleasing
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         panic!("oops");
@@ -13,33 +12,36 @@ fn main() {
     if &args[1] == "r" {
         let my_time = time::OffsetDateTime::now_utc().to_string();
         println!("{}", my_time);
-        write_to_file(my_time);
+        write_to_file(my_time)?;
     } else if &args[1] == "l" {
-        println!("{}", read_last_from_file());
+        println!("{}", read_last_from_file()?);
     } else if &args[1] == "a" {
-        println!("{}", read_all_from_file());
+        println!("{}", read_all_from_file()?);
     } else {
         panic!("wrong arguments");
     }
+
+    Ok(())
 }
 
-fn read_last_from_file() -> String {
-    let file = File::open("times.txt").unwrap();
+fn read_last_from_file() -> io::Result<String> {
+    let file = File::open("times.txt")?;
     let lines = BufReader::new(file).lines();
-    let last_el = lines.last().unwrap().unwrap();
+    let last_el = lines.last().unwrap();
 
-    return last_el;
+    last_el
 }
 
-fn read_all_from_file() -> String {
-    fs::read_to_string("times.txt").unwrap()
+fn read_all_from_file() -> io::Result<String> {
+    fs::read_to_string("times.txt")
 }
 
-fn write_to_file(date: String) {
+fn write_to_file(date: String) -> io::Result<()> {
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
-        .open("times.txt")
-        .unwrap();
-    file.write((date + "\n").as_bytes()).unwrap();
+        .open("times.txt")?;
+    file.write((date + "\n").as_bytes())?;
+
+    Ok(())
 }
