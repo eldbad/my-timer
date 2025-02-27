6b2310c4704ac537b892c4b01b222317bae8da9b
Eldar Badalbeyli <eld.bad@yandex.com>
2025-02-27 19:21:41 +0500

    Error modification and naming

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,38 @@
 use std::{
-    env, error, fmt,
+    env, fmt,
     fs::{self, File, OpenOptions},
     io::{self, BufRead, BufReader, Write},
 };
 
-// why pub tho?
 #[derive(Debug)]
-pub struct NoLastRecordError;
-
-impl fmt::Display for NoLastRecordError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "no last record found in file")
-    }
-}
-
-impl error::Error for NoLastRecordError {}
-
-#[derive(Debug)]
-pub enum OopsError {
+pub enum TimerError {
     NoLastRecordError,
+    WrongNumberOfArguments,
+    WrongArgument,
     IoError(io::Error),
 }
 
-impl From<io::Error> for OopsError {
-    fn from(error: io::Error) -> Self {
-        OopsError::IoError(error)
+impl fmt::Display for TimerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TimerError::NoLastRecordError => write!(f, "no last record found in file"),
+            TimerError::IoError(err) => write!(f, "{}", err),
+            TimerError::WrongNumberOfArguments => write!(f, "wrong number of arguments given"),
+            TimerError::WrongArgument => write!(f, "wrong argument"),
+        }
     }
 }
 
-fn main() -> Result<(), OopsError> {
+impl From<io::Error> for TimerError {
+    fn from(error: io::Error) -> Self {
+        TimerError::IoError(error)
+    }
+}
+
+fn main() -> Result<(), TimerError> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        panic!("oops");
+        return Err(TimerError::WrongNumberOfArguments);
     }
     if &args[1] == "r" {
         let my_time = time::OffsetDateTime::now_utc().to_string();
@@ -42,19 +43,19 @@ fn main() -> Result<(), OopsError> {
     } else if &args[1] == "a" {
         println!("{}", read_all_from_file()?);
     } else {
-        panic!("wrong arguments");
+        return Err(TimerError::WrongArgument);
     }
 
     Ok(())
 }
 
-fn read_last_from_file() -> Result<String, OopsError> {
+fn read_last_from_file() -> Result<String, TimerError> {
     let file = File::open("times.txt")?;
     let lines = BufReader::new(file).lines();
     if let Some(last_el) = lines.last() {
         Ok(last_el?)
     } else {
-        Err(OopsError::NoLastRecordError)
+        Err(TimerError::NoLastRecordError)
     }
 }
 
@@ -62,12 +63,12 @@ fn read_all_from_file() -> io::Result<String> {
     fs::read_to_string("times.txt")
 }
 
-fn write_to_file(date: String) -> io::Result<()> {
+fn write_to_file(value: String) -> io::Result<()> {
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
         .open("times.txt")?;
-    file.write((date + "\n").as_bytes())?;
+    file.write((value + "\n").as_bytes())?;
 
     Ok(())
 }
