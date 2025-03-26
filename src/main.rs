ddb641e358a19c800d88beece76e5bb01089be8b
Eldar Badalbeyli <eld.bad@yandex.com>
2025-03-26 21:10:24 +0500

    Misc refactor

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,13 @@ mod time_error;
 
 use crate::time_error::TimerError;
 
-use std::{
-    env,
-    fs::{self, File, OpenOptions},
-    io::{self, BufRead, BufReader, Write},
-};
+use std::env;
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, BufRead, BufReader, Write};
 
 use time::format_description::BorrowedFormatItem;
 use time::{macros::format_description, OffsetDateTime};
 
-// Can we omit subseconds?
 static FORMAT: &[BorrowedFormatItem<'_>] = format_description!(
     "[year]-[month]-[day] [hour]:[minute]:[second] \
     [offset_hour sign:mandatory]:[offset_minute]:[offset_second]"
@@ -21,6 +18,7 @@ static FORMAT: &[BorrowedFormatItem<'_>] = format_description!(
 // 1. add cli crate
 // 2. create times.txt if not found
 // 3. get dates
+// 4. times.txt to $HOME/.config/my-timer/
 fn main() -> Result<(), TimerError> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -28,6 +26,9 @@ fn main() -> Result<(), TimerError> {
     }
 
     if &args[1] == "r" {
+        // TODO:
+        // 1. all other commands should return NoLastRecord
+        // 2. should create a directory my-timer and file times.txt corresponding to operating system
         add_new_time()?;
     } else if &args[1] == "l" {
         println!("{}", read_last_from_file()?);
@@ -82,17 +83,14 @@ fn write_to_file(value: String) -> io::Result<()> {
 
 fn last_time() -> Result<String, TimerError> {
     let last_time = read_last_from_file()?;
-    // TODO: fix incorrect hour description
-    // Because [hour] doesn't recognize one
-    // literal, only two
-    let new_last_time = OffsetDateTime::parse(last_time.as_str(), &FORMAT)?; // DELETE UNWRAP
+    let new_last_time = OffsetDateTime::parse(last_time.as_str(), &FORMAT)?;
     let result = OffsetDateTime::now_utc() - new_last_time;
 
     let mut string_result = result.to_string();
     let seconds_position = string_result
         .chars()
         .position(|c| c == 's')
-        .ok_or(TimerError::ParseDuration)?; // DELETE ANOTHER UNWRAP
+        .ok_or(TimerError::ParseDuration)?;
     string_result.truncate(seconds_position + 1);
 
     Ok(string_result)
